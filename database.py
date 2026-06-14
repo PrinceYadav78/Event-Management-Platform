245fcc96cc8b712f3494a01bb7899103311e8d20
Prince Yadav <80159695+PrinceYadav78@users.noreply.github.com>
2026-06-14 11:33:38 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,14 +3,24 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./school_events.db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+if SQLALCHEMY_DATABASE_URL.startswith("postgresql"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        poolclass=NullPool,
+    )
+else:
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        pool_pre_ping=True,
+        pool_recycle=300,
+        connect_args={"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
